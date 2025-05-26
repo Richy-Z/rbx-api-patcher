7bdd5c8221717aaebdb075515e994504d06c8996
unknownsea <seasoank@gmail.com>
2025-05-25 17:39:44 -0700

    fixed false positive

--- src/islclosure.lua
+++ src/islclosure.lua
@@ -2,9 +2,7 @@ local function islclosure(func)
     if typeof(func) ~= "function" then return false end
 
     local info = debug.getinfo(func)
-    -- A Luau-compiled function typically has a `source` starting with "@"
-    -- C functions may have "[C]" as their source
-    return info and info.source and not info.source:find("^%[C%]")
+    return info and info.what ~= "C"
 end
 
 return islclosure
